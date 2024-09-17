7028645af475bef60d3b75a384d809f9fafcfe89
Hector Garcia <hmgarcia06@gmail.com>
2024-09-17 12:46:24 -0400

    Testing edge functions in vercel

--- api/edge.ts
+++ api/edge.ts
@@ -1,5 +1,14 @@
-export const config = {
-  runtime: 'edge',
-}
+import { createGreeting  } from '../utils'
 
-export default (req) => new Response('Hello world!')
+
+export const config = {
+	runtime: 'edge',
+  }
+  
+  export default async (req: any) => {
+	createGreeting('world')
+	  const response = new Response(JSON.stringify({ message: 'Hello world!' }), {
+		  headers: { 'Content-Type': 'application/json' },
+	  })
+	  return response
+  }
---
+++ utils/index.ts
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from 'uuid'
+
+// Generate a greeting with a unique ID
+export const createGreeting = (name: string) => {
+  const id = uuidv4()
+  return `Hello, ${name}! Your ID is ${id}.`
+}
