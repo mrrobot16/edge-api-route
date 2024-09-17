0ea7279cf5b5fdc3afcf584264bcf7fa7481e330
Hector Garcia <hmgarcia06@gmail.com>
2024-09-17 16:11:58 -0400

    Testing issue with json.stringify

--- api/health/[step].ts
+++ api/health/[step].ts
@@ -7,13 +7,13 @@ export const config = {
 
 export default async function handler(req: Request) {
 	// console.log('req.params', req)
-	console.log('req.body', await req.json())
-	const test = (await axios.get('http://localhost:3000/api/test'))
+	// console.log('req.body', await req.json())
+	// const test = (await axios.get('http://localhost:3000/api/test'))
 	const data = {
 		message: 'Hello world!',
 		greeting: createGreeting('Hector'),
 		greetingV2: createGreetingV2('John'),
-		test: test.data,
+		// test: test.data,
 	};
 
 	const headers = {
