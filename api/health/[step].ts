70b475eab347f4638d5e07510ea982554b3868a8
Hector Garcia <hmgarcia06@gmail.com>
2024-09-17 13:55:54 -0400

    Tested removing modules for middleware

--- api/health/[step].ts
+++ api/health/[step].ts
@@ -1,18 +1,19 @@
-import { createGreeting } from 'lib/utils';
-import { createGreetingV2 } from 'lib/utils-v2';
-import axios from 'axios';
+// import { createGreeting } from 'lib/utils';
+// import { createGreetingV2 } from 'lib/utils-v2';
+// import axios from 'axios';
 export const config = {
   runtime: 'edge',
 };
 
 export default async function handler(req: Request) {
+	// console.log('req.params', req)
 	console.log('req.body', await req.json())
-	const test = (await axios.get('http://localhost:3000/api/test'))
+	// const test = (await axios.get('http://localhost:3000/api/test'))
 	const data = {
 		message: 'Hello world!',
-		greeting: createGreeting('Hector'),
-		greetingV2: createGreetingV2('John'),
-		test: test.data,
+		// greeting: createGreeting('Hector'),
+		// greetingV2: createGreetingV2('John'),
+		// test: test.data,
 	};
 
 	const headers = {
